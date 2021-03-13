90e2e44f3ab12c14c4a844f0c1c81c78cd10a934
Jumpei Ogawa <phanective@gmail.com>
2021-03-14 08:05:21 +0900

    feat(backend): make directories unconfigurable

--- modules/backend/config.js
+++ modules/backend/config.js
@@ -1,12 +1,12 @@
 "use strict";
 
 const fs = require("fs");
-const path = require("path");
+const { resolve } = require("path");
 
 const config = {
-  database: "",
-  imageFolder: "",
-  thumbnailFolder: "",
+  database: resolve(__dirname, "./database"),
+  imageFolder: resolve(__dirname, "./images"),
+  thumbnailFolder: resolve(__dirname, "./thumb"),
   port: 8000,
 };
 
@@ -20,25 +20,7 @@ function checkAccess(file) {
   return true;
 }
 
-exports.initConfig = function(extConfig) {
-  let fileConfig = {};
-  try {
-    var configFilePath = path.resolve(__dirname, "../config.json");
-    const configContents = fs.readFileSync(configFilePath, "utf8");
-    console.log(`Found configuration file : ${configFilePath}`);
-    try {
-      fileConfig = JSON.parse(configContents);
-    } catch(e) {
-      console.log(`Invalid data: ${configFilePath} does not contain a valid JSON string.`);
-    }
-  } catch(e) {
-    console.log(`No configuraton file found : ${configFilePath}`);
-  }
-  extConfig = extConfig || fileConfig;
-  config.database = path.resolve(__dirname, "./", extConfig.database || "database");
-  config.imageFolder = path.resolve(__dirname, "./", extConfig.imageFolder || "images");
-  config.thumbnailFolder = path.resolve(__dirname, "./", extConfig.thumbnailFolder || "thumb");
-  config.port = extConfig.port || 3000;
+exports.initConfig = function() {
   // Check access permissions
   if(!checkAccess(config.database)) {
     console.error(`The database folder ${config.database} can't be accessed. Ensure that the folder is created and that the user has enough permissions to access it.`);
